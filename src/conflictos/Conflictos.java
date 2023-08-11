826f16bff3dbe9f880a1d0db9030ccf18ad0b59f
Ericku20 <erickalfaro119@gmail.com>
2023-08-11 16:02:22 -0600

    clase conflicto
    Conflicto

--- src/conflictos/Conflictos.java
+++ src/conflictos/Conflictos.java
@@ -10,11 +10,9 @@ package conflictos;
  */
 public class Conflictos {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
